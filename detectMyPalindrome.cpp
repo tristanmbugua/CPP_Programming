2b4607fb0e0c98b3e9f30dadcd17e74a5faaf9e4
tristanmbugua <128255762+tristanmbugua@users.noreply.github.com>
2024-01-06 10:04:51 -0500

    Palindrome question complete.

--- detectMyPalindrome.cpp
+++ detectMyPalindrome.cpp
@@ -10,30 +10,33 @@
 std::string testString = "Error";
 bool loopContinue = true;
 
-
-int main() {
+int main()
+{
     std::cout << "Hello. Welcome to Detect My Palindrome!" << std::endl;
-    while (loopContinue) {
-        std::cout << "Please enter a potential palindrome." << std::endl;
-        std::cin >> testString;
-        const int stringLength = testString.length() - 1;
-        bool notAPalindrome = false;
-        for (int i = 0; i < stringLength; i++) {
-            if (i <= (testString.length() / 2)) {
-                if (!(testString[i] == testString[stringLength - i])) {
-                    notAPalindrome = true;
-                    break;
-                }  
+
+    std::cout << "Please enter a potential palindrome." << std::endl;
+    std::cin >> testString;
+    const int stringLength = testString.length() - 1;
+    bool notAPalindrome = false;
+    for (int i = 0; i < stringLength; i++)
+    {
+        if (i <= (testString.length() / 2))
+        {
+            if (!(testString[i] == testString[stringLength - i]))
+            {
+                notAPalindrome = true;
+                break;
             }
         }
-        if (notAPalindrome) {
-            std::cout << "This word is not a palindrome." << std::endl;
-        } else {
-            std::cout << "This word is a palindrome!" << std::endl;
-        }
-        //Continue loop mechanism.
-        std::cout << "Would you like to try again?" << std::endl;
-            //Complete this.
     }
+    if (notAPalindrome)
+    {
+        std::cout << "This word is not a palindrome." << std::endl;
+    }
+    else
+    {
+        std::cout << "This word is a palindrome!" << std::endl;
+    }
+
     return 0;
 }
